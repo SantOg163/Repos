36fd59de062e73bc9791ef534663514db9e26e62
SanT OG <ssanturan79@gmail.com>
2022-02-01 23:42:32 +0300

    Delegate

---
+++ StaticDataAndMembers/SimpleDelegate/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDelegate
+{
+    internal class Car
+    {
+        // Данные состояния,
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName { get; set; }
+        // Исправен ли автомобиль?
+        private bool carlsDead;
+        // Конструкторы класса,
+        public Car() { }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+
+        // 1. Определить тип делегата.
+        public delegate void CarEngineHandler(string msgForCaller);
+        //2. Определить переменную-член этого типа делегата.
+        private CarEngineHandler listOfHandlers;
+        // 3. Добавить регистрационную функцию для вызывающего кода.
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            if(listOfHandlers == null)
+               listOfHandlers = methodToCall;
+            else
+                Delegate.Combine(listOfHandlers, methodToCall);
+        }
+        // 4. Реализовать метод Accelerate () для обращения к списку
+        // вызовов делегата в подходящих обстоятельствах.
+        public void Accelerate(int delta)
+        {
+            if (carlsDead)
+            {
+                if (listOfHandlers != null)
+                    Console.WriteLine("Car is dead");
+            }
+            else
+                CurrentSpeed += delta;
+            // Автомобиль почти сломан?
+            if(MaxSpeed - CurrentSpeed == 10 && listOfHandlers != null)
+                Console.WriteLine("Почти сломалась");
+            if (CurrentSpeed > MaxSpeed)
+                carlsDead = true;
+            else
+                Console.WriteLine("CurrentSpeed = " + CurrentSpeed);
+        }
+    }
+}
---
+++ StaticDataAndMembers/SimpleDelegate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDelegate
+{
+    internal class Program
+    {
+        public static void OnCarEngineEvent(string msg)
+        {
+            Console.WriteLine(" Message From Car Object ");
+            Console.WriteLine("=> {0}", msg);
+        }
+        public static void OnCarEngineEvent2(string msg)
+        {
+            Console.WriteLine("=> "+ msg.ToUpper());
+        }
+        static void Main(string[] args)
+        {
+            // Создать объект Car.
+            Car car = new Car("SanT",100,10);
+            //есть ДВА метода, которые будут
+            // вызываться Саг при отправке уведомлений.
+            car.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent));
+            car.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent2));
+            Car.CarEngineHandler c = new Car.CarEngineHandler(OnCarEngineEvent);
+            c += OnCarEngineEvent2;
+            foreach(Delegate d in c.GetInvocationList())
+                Console.WriteLine(d.Method.Name);
+            for (int i = 0; i < 6; i++)
+                car.Accelerate(20);
+            Console.ReadKey();
+        }
+    }
+}
