9780bf2d396ec57400a22d7b514a021f2ddc52b4
SanT OG <ssanturan79@gmail.com>
2022-03-31 23:53:10 +0300

    Error

---
+++ WpfValidations/WpfValidations/Models/EntityBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfValidations.Models
+{
+    internal class EntityBase : INotifyDataErrorInfo
+    {
+        public bool HasErrors => throw new NotImplementedException();
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WpfValidations/WpfValidations/Models/InventoryPartial.cs
+++ WpfValidations/WpfValidations/Models/InventoryPartial.cs
@@ -4,28 +4,89 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using System.Collections;
 
 namespace WpfValidations.Models
 {
-    public partial class Inventory : IDataErrorInfo
+    public partial class Inventory : IDataErrorInfo, INotifyDataErrorInfo
     {
-        private string _error;
-        public string Error => _error;
+        private readonly Dictionary<string, List<string>> _errors = new Dictionary<string, List<string>>();
+        public bool HasErrors => _errors.Count != 0;
+
+        public string Error => throw new NotImplementedException();
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        private void OnErrorsChanged(string propertyName) => ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (string.IsNullOrEmpty(propertyName))
+                return _errors.Values;
+            return _errors.ContainsKey(propertyName) ? _errors[propertyName] : null;
+        }
+        private void AddError(string propertyName, string error) => AddErrors(propertyName, new List<string> { error });
+        private void AddErrors(string propertyName, IList<string> errors)
+        {
+            var changed = false;
+            if (!errors.Contains(propertyName))
+            {
+                _errors.Add(propertyName, new List<string>());
+                changed = true;
+            }
+            foreach (var error in errors)
+            {
+                if (_errors[propertyName].Contains(error))
+                    continue;
+                _errors[propertyName].Add(error);
+                changed = true;
+            }
+            if (changed)
+                OnErrorsChanged(propertyName);
+        }
+        protected void ClearErrors(string propertyName = "")
+        {
+            if (string.IsNullOrEmpty(propertyName))
+                _errors.Clear();
+            else
+                _errors.Remove(propertyName);
+            OnErrorsChanged(propertyName);
+        }
+        /*IDataErrorInfo*/
+        //private string _error;
+        //public string Error => _error;
 
         public string this[string columnName]
         {
             get
             {
+                bool hasError = false;
                 switch (columnName)
                 {
                     case nameof(CarId):
                         break;
                     case nameof(Make):
+                        hasError = CheckMakeAndColor();
                         if (Make == "ModelT")
-                            return "Слишком старая модель";
-                        return CheckMakeAndColor();
+                        {
+                            hasError = true;
+                            AddError(nameof(Make), "to old");
+                        }
+                        if(!hasError)
+                        {
+                            // Логика не безупречна, а просто иллюстрирует паттерн
+                            ClearErrors(nameof(Make));
+                            ClearErrors(nameof(Color));
+                        }
+                        break;
                     case nameof(Color):
-                        return CheckMakeAndColor();
+                        hasError = CheckMakeAndColor();
+                        if(!hasError)
+                        {
+                            ClearErrors(nameof(Make));
+                            ClearErrors(nameof(Color));
+                        }
+                        break;
                     case nameof(PetName):
                         break;
                 }
@@ -33,11 +94,15 @@ namespace WpfValidations.Models
             }
         }
 
-        internal string CheckMakeAndColor()
+        internal bool CheckMakeAndColor()
         {
             if (Make == "Chevy" && Color == "Pink")
-                return "модель в таком цвете не поставляется";
-            return string.Empty;
+            {
+                AddError(nameof(Make), $"{Make}, 's don't come in {Color}");
+                AddError(nameof(Color), $"{Make}, 's don't come in {Color}");
+                return true;
+            }
+            return false;
         }
     }
 }
